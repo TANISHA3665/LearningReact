2cafaf2de7cd6c43fbab521658eca37557969e46
Tanisha <jaiswaltanisha11@gmail.com>
2023-05-06 01:08:39 +0530

    learnt functional and class-based components

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Functional Component
+
+// function App()  {
+//   return <h1>This is my functional component!</h1>
+// }
+
+// Class-based component
+
+class App extends React.Component   // inheritance
+{ 
+  render()
+  {
+    return <h1>This is my class-based component</h1>
+  }
+}
+
+export default App;
